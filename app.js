82b1aef976a3ac4fceac537431c669299cebd88a
Rex Zibit <jack.problems@ssh.github.com>
2022-01-03 14:43:01 -0800

    BTC added to convert over the what nots

--- app.js
+++ app.js
@@ -1,29 +1,3 @@
-/* find it on blockchain.info/address
-The brain wallet of whyyoudbemurderd
-addy : 1MC3gBYsYwQpiEf1LhyZSei3zBA1ETveiK
-PrivateKey : Kx7sJES3PTHUqodQftQyb5gBmfXwRHkdrPRXwpfRRtGPPvjK1vg3
-PrivateKey : L5nAy7eUzuTKo2jRTPJihtm9zsDakaTxU1YgUcY2cdFaeTycH1Zn
-
-addy : 1C6MFHidpayxquCmU6MJrtNyMwABQ4FpS3
-PrivateKey : L4gSQMMRBj2WmiYFrbgpFNh9Vrnn7rKHQaK9EL2ZNmpWV1vKLRgp
-
-#### As far as Deprecation warnings go
-node  --trace-deprecation app.js
-go
-complete shootscoots
-(node:308387) [DEP0005] DeprecationWarning: Buffer() is deprecated due to security and usability issues. Please use the Buffer.alloc(), Buffer.allocUnsafe(), or Buffer.from() methods instead.
-    at showFlaggedDeprecation (buffer.js:194:11)
-    at new Buffer (buffer.js:281:3)
-    at /var/www/wwwhtml/udemy/fftne/bmvx/Node-Btc-Wallet/app.js:29:17
-    at Layer.handle [as handle_request] (/var/www/wwwhtml/udemy/fftne/bmvx/Node-Btc-Wallet/node_modules/express/lib/router/layer.js:95:5)
-    at next (/var/www/wwwhtml/udemy/fftne/bmvx/Node-Btc-Wallet/node_modules/express/lib/router/route.js:137:13)
-    at Route.dispatch (/var/www/wwwhtml/udemy/fftne/bmvx/Node-Btc-Wallet/node_modules/express/lib/router/route.js:112:3)
-    at Layer.handle [as handle_request] (/var/www/wwwhtml/udemy/fftne/bmvx/Node-Btc-Wallet/node_modules/express/lib/router/layer.js:95:5)
-    at /var/www/wwwhtml/udemy/fftne/bmvx/Node-Btc-Wallet/node_modules/express/lib/router/index.js:281:22
-    at Function.process_params (/var/www/wwwhtml/udemy/fftne/bmvx/Node-Btc-Wallet/node_modules/express/lib/router/index.js:341:12)
-    at next (/var/www/wwwhtml/udemy/fftne/bmvx/Node-Btc-Wallet/node_modules/express/lib/router/index.js:275:10)
-
-*/
 
 
 var express = require("express");
@@ -79,9 +53,11 @@ app.post("/wallet",function(req,res){
     var brainsrc = req.body.brainsrc;
     console.log("complete "+ brainsrc);
     
-    brainWallet(brainsrc, function(private,address){
-        res.send("The brain wallet of "+ brainsrc + "<br> addy : "+ address + "<br> PrivateKey : " + private);
-    });
+
+        brainWallet(brainsrc, function(private,address){
+            res.send("The brain wallet of "+ brainsrc + "<br> addy : "+ address + "<br> PrivateKey : " + private);
+        })
+    
 
 });
 
